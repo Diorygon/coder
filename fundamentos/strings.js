49e66205d1828655838d9675b5733f16cec880ab
Diego Mello <diegomll4@gmail.com>
2021-03-31 21:40:06 -0300

    Estudos 31/03

---
+++ fundamentos/strings.js
@@ -0,0 +1,16 @@
+const nome = "D13G0"
+
+console.log(nome.charAt(0))
+console.log(nome.charAt(1))
+console.log(nome.charAt(2))
+console.log(nome.charAt(3))
+console.log(nome.charAt(4))
+
+console.log(nome.charCodeAt(3))
+console.log(nome.indexOf('G'))
+console.log(nome.substring(1))
+console.log(nome.substring(2,4))
+
+console.log('Nome '.concat(nome).concat("!"))
+console.log(nome.replace(1,'I'))
+
