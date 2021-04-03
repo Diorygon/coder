3d6eb230b9367c1110329548c0295578c2352f7d
Diego Mello <diegomll4@gmail.com>
2021-04-02 23:14:24 -0300

    Estudos 02/04

---
+++ fundamentos/ternario.js
@@ -0,0 +1,10 @@
+//const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+//ou 
+
+const resultado = nota => {
+    return nota >= 7 ? 'Aprovado' : 'Reprovado'
+}
+
+console.log(resultado(7.5))
+console.log(resultado(4))
