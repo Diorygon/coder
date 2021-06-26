3cb774979199488ff289d5d0b63b2d34f335af82
Diego Mello <diegofemello@gmail.com>
2021-06-25 22:29:19 -0300

    .

--- funcao/paramPadrao.js
+++ funcao/paramPadrao.js
@@ -5,7 +5,6 @@ function soma1(a, b, c){
     c = c || 1
     return a + b + c
 }
-
 console.log(soma1(), soma1(3), soma1(1,2,3), soma1(0,0,0))
 
 
@@ -14,9 +13,13 @@ console.log(soma1(), soma1(3), soma1(1,2,3), soma1(0,0,0))
 function soma2(a,b,c){
     a = a !== undefined ? a : 1
     b = 1 in arguments ? b : 1
-    c = isNAN(c) ? 1 : c
+    c = isNaN(c) ? 1 : c
 
     return a + b + c
 }
-
 console.log(soma2(), soma2(3), soma2(1,2,3), soma2(0,0,0))
+
+//valor padrao es2015
+function soma3(a =1, b = 1, c = 1)
+console.log(soma3(), soma3(3), soma3(1,2,3), soma3(0,0,0))
+
