8305d392ae4bc98aa3219fc9e2bfd61c91889530
Diego Mello <diegofemello@gmail.com>
2021-07-03 13:06:36 -0300

    gulp

---
+++ gulp/css/gulpfile.js
@@ -0,0 +1,27 @@
+const {series, parallel} = require('gulp')
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('node-sass'));
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+
+function transformacaoCSSMin(){
+    return gulp.src('src/sass/index.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(uglifycss({"uglyComments": true}))
+        .pipe(concat('estilo.min.css'))
+        .pipe(gulp.dest('build/css'))
+}
+
+function transformacaoCSS(){
+    return gulp.src('src/sass/index.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('estilo.css'))
+        .pipe(gulp.dest('build/css'))
+}
+
+function copiarHTML(){
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest('build'))
+}
+
+exports.default = parallel(transformacaoCSSMin, transformacaoCSS, copiarHTML)
